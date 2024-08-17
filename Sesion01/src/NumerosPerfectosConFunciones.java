9be3c97763b38813e04d02c73ec392503a4bd086
PKTaKeshi1 <164096197+PKTaKeshi1@users.noreply.github.com>
2024-08-17 07:43:05 -0500

    Add files via upload

---
+++ Sesion01/src/NumerosPerfectosConFunciones.java
@@ -0,0 +1,29 @@
+package lab01;
+import java.util.Scanner;
+
+public class NumerosPerfectosConFunciones{
+	
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Introduce un numero: ");
+		int n = scanner.nextInt();
+		imprimirNumerosPerfectos(n);
+	}
+	public static boolean esPerfecto(int num) {
+		int sumaDivisores = 0;
+		
+		for(int i= 1; i<=num/2; i++) {
+			if(num%i==0) {
+				sumaDivisores +=i;
+			}
+		}
+		return sumaDivisores==num;
+	}
+	public static void imprimirNumerosPerfectos(int n) {
+		for (int num=1; num<n;num++) {
+			if(esPerfecto(num)) {
+				System.out.println(num + " Es un numero perfecto.");
+			}
+		}
+	}
+}
