2cef7b8afe8a414ac272e9880a7592b4a7ddf68e
PKTaKeshi1 <164096197+PKTaKeshi1@users.noreply.github.com>
2024-08-17 07:57:33 -0500

    Add files via upload

---
+++ Sesion01/src/AnalisisArreglo.java
@@ -0,0 +1,33 @@
+package lab01;
+
+import java.util.Scanner;
+
+public class AnalisisArreglo {
+
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Introduce el tammaño del arreglo: ");
+		int n =scanner.nextInt();
+		
+		int[] arreglo = new int[n];
+		System.out.println("Introduce los elementeos del arreglo: ");
+		leerArreglo(scanner, arreglo);
+		
+		double promedio = calcularPromedio(arreglo);
+		System.out.println("El promedio de los elemetos es: " + promedio);
+		
+	}
+	public static void leerArreglo(Scanner scanner, int [] arreglo) {
+		for (int i=0; i<arreglo.length;i++) {
+			arreglo[i]=scanner.nextInt();
+		}
+	}
+	public static double calcularPromedio(int[]arreglo) {
+		int suma=0;
+		for(int num: arreglo) {
+			suma+=num;
+		}
+		return(double) suma/arreglo.length;
+	}
+
+}
