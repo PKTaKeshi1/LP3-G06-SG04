7c8d4ff4cad4f1746609251177223b2521a273d5
PKTaKeshi1 <164096197+PKTaKeshi1@users.noreply.github.com>
2024-08-31 08:09:56 -0500

    Update Contador.java

--- Sesion02/src/Actividades/Act02/Contador.java
+++ Sesion02/src/Actividades/Act02/Contador.java
@@ -1,1 +1,26 @@
+package Sesion02;
+
+public class Contador {
+	static int acumulador = 0;
+	private int valor;
+	
+	public static int acumulador() {
+		return acumulador;
+	}
+	
+	public Contador(int valor) {
+		this.valor = valor;
+		acumulador += valor;
+	}
+	
+	public void inc() {
+		valor++;
+		acumulador++;
+	}
+	
+	public int getValor() {
+		return this.valor;
+	}
+
+}
 
