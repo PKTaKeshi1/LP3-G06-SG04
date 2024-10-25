7d5c2b8e4c70d7572840c8a110ef1688fddf9ba9
PKTaKeshi1 <164096197+PKTaKeshi1@users.noreply.github.com>
2024-10-24 22:52:22 -0500

    Delete Sesion07/src directory

--- Sesion07/src/Actividades/Actividad05/AppContactos.java
+++
@@ -1,8 +0,0 @@
-package Sesion07.src.Actividades.Actividad05;
-
-public class AppContactos {
-    public static void main(String[] args) {
-        Agenda agenda = new Agenda();
-        agenda.bucle();
-    }
-}
