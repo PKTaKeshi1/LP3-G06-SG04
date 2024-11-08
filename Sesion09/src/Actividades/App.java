49af89f54c41836f530b02d08d023c9de9b548ec
PKTaKeshi1 <164096197+PKTaKeshi1@users.noreply.github.com>
2024-11-08 08:43:01 -0500

    Add files via upload

---
+++ Sesion09/src/Actividades/App.java
@@ -0,0 +1,15 @@
+package Sesion09.src.Actividades;
+
+// Archivo “App.java” 
+// Prueba de LabelFrame 
+import javax.swing.JFrame; 
+public class App  
+{ 
+    public static void main(String[] args) 
+        {  
+            LabelFrame marcoEtiqueta = new LabelFrame(); 
+            marcoEtiqueta.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+            marcoEtiqueta.setSize(260, 180); 
+            marcoEtiqueta.setVisible(true); 
+        } 
+}// Fin de App Prueba Label 
