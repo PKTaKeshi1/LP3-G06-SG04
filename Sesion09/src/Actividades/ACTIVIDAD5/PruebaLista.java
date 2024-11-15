6956bb7647845f6b9310d8a46e5aafc78ff0764e
SERGIOAYALAARCE <142816218+SERGIOAYALAARCE@users.noreply.github.com>
2024-11-14 22:23:13 -0500

    Add files via upload

---
+++ Sesion09/src/Actividades/ACTIVIDAD5/PruebaLista.java
@@ -0,0 +1,10 @@
+package actividad5;
+import javax.swing.JFrame;
+public class PruebaLista {
+	public static void main(String[] args) {
+		MarcoLista marcoLista = new MarcoLista(); // crea objeto MarcoLista
+		marcoLista.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		marcoLista.setSize(350, 150);
+		marcoLista.setVisible(true);
+	}
+}
