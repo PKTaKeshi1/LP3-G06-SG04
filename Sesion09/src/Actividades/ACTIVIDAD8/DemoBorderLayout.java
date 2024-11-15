d7fa79cb707111278e1755dba114cba2c70c96f3
SERGIOAYALAARCE <142816218+SERGIOAYALAARCE@users.noreply.github.com>
2024-11-14 22:54:33 -0500

    Add files via upload

---
+++ Sesion09/src/Actividades/ACTIVIDAD8/DemoBorderLayout.java
@@ -0,0 +1,12 @@
+package ACTIVIDAD8;
+import javax.swing.JFrame;
+public class DemoBorderLayout
+{
+    public static void main(String[] args)
+    {
+        MarcoBorderLayout marcoBorderLayout = new MarcoBorderLayout();
+        marcoBorderLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoBorderLayout.setSize(300, 200);
+        marcoBorderLayout.setVisible(true);
+    }
+} // fin de la clase DemoBorderLayout
---
+++ Sesion09/src/Actividades/ACTIVIDAD8/package-info.java
@@ -0,0 +1,1 @@
+package ACTIVIDAD8;
